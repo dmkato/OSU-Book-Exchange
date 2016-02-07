c0a7fc41ae6c7bd3891c50f90f294d399196953a
dmkato <dmkato@gmail.com>
2016-02-06 18:33:19 -0800

    Added logout button, But it doesn't work right....

--- login.php
+++ login.php
@@ -4,15 +4,19 @@
 ?>
 <?php include("_header.php");?>
 <?php 
-	$onidid = checkAuth(true)
+    $onidid = checkAuth(true);
 		
 	//Check For Onid ID in SQL to Retrieve account info
 	
 	?>
 	<h3>Login</h3></br>
 
-	<div class="panel panel-default"><div class="panel-body">You're Logged in as <?php echo "$onidid"; ?></div></div>
-	
-	<!-- Add Logout Button-->
+	<div class="panel panel-default"><div class="panel-body">
+        <p>You're Logged in as <?php echo "$onidid"; ?></p>
+
+        <p><button onclick="location.href = 'logout.php';" type="button" class="btn btn-default">logout?</button></p>
+
+        <!-- find out how to 'On Click' in javascript to add 'active' class on press -->
+    </div></div>
 
 <?php include("_footer.php");?>
